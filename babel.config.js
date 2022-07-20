1dd5c02b323fc3ae7311d4217b8d10770a40b376
Genereux-akotenou <genereux.akotenou@gmail.com>
2022-07-20 13:39:20 +0100

    Foxed: add url loader config in babel file

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,4 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
-  plugins: [
-    [
-      "inline-import-data-uri",
-      {
-        "extensions": ["png", "jpg", "jpeg", "gif", "svg", "pdf"],
-        "limit": 10000
-      }
-    ]
-  ]
 }
