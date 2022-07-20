99cf2caa1332c0e53ca4d45040cb61196670fc9e
Genereux-akotenou <genereux.akotenou@gmail.com>
2022-07-20 12:47:09 +0100

    Fixed: url-loader

--- babel.config.js
+++ babel.config.js
@@ -3,14 +3,16 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ],
   rules: [
-    {// remove the block
-      test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-      loader: 'url-loader',
-      options: {
-        limit: 10000,
-        // eslint-disable-next-line no-undef
-        name: utils.assetsPath('img/[name].[hash:7].[ext]')
-      }
+    {
+      test: /\.(png|jpg|gif|svg)$/i,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+          },
+        },
+      ],
     },
   ]
 }
